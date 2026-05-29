b99617055c12d669a4d4b6c26ceeaeecbbda3107
Nojan-Devkota <nojan.devkota@gmail.com>
2026-05-29 16:56:34 -0500

    Add v1 API router aggregating endpoints

---
+++ backend/api/v1/router.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from api.v1 import courses, enrollments, students
+
+api_router = APIRouter()
+api_router.include_router(students.router)
+api_router.include_router(courses.router)
+api_router.include_router(enrollments.router)
